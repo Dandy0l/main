994244052b66ff68261ef4840bf66402a9f4ddc8
Dandy <slada5026@mail.ru>
2023-04-06 20:39:55 +0300

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a special test progect for mine exp
---
+++ main.py
@@ -0,0 +1,18 @@
+def func(x):
+    m = []
+    for i in range(1, int(x ** 0.5) + 1):
+        if x == i * i:
+            if i % 2 == 0:
+                m += [i]
+        elif x % i == 0:
+            if i % 2 == 0:
+                m += [i]
+            if (x // i) % 2 == 0:
+                m += [x // i]
+    return m
+
+for num in range(110203, 110246):
+    answ = func(num)
+    if len(answ) == 4:
+        answ.sort()
+        print(answ)
